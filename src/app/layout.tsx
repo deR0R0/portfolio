dd79472b869e89fda8bf4ce8dff1d97966bff08a
deR0R0 <rorozee1@gmail.com>
2025-07-10 10:02:47 -0400

    fix: fix the open graph image

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,22 @@ import "./globals.css";
 export const metadata: Metadata = {
   title: "Robert Zhao",
   description: "Robert Zhao's Personal Portfolio Website",
+  openGraph: {
+    title: "Robert Zhao",
+    description: "Robert Zhao's Personal Portfolio Website",
+    url: "https://robertzhao.dev",
+    siteName: "Robert Zhao",
+    images: [
+      {
+        url: "https://robertzhao.dev/opengraph-image.png",
+        width: 1200,
+        height: 630,
+        alt: "Robert Zhao's Portfolio Website",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  }
 };
 
 export default function RootLayout({ children, }: Readonly<{ children: React.ReactNode;}>) {
